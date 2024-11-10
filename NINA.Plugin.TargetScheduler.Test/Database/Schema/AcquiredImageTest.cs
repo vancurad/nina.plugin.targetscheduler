83cba2daf5c10533ae4efd349bd3cc041455e84f
Tom Palmer <palmer.tom@gmail.com>
2024-11-10 06:11:03 -0500

    db testing

---
+++ NINA.Plugin.TargetScheduler.Test/Database/Schema/AcquiredImageTest.cs
@@ -0,0 +1,15 @@
+﻿using NINA.Plugin.TargetScheduler.Database.Schema;
+using NUnit.Framework;
+
+namespace NINA.Plugin.TargetScheduler.Test.Database.Schema {
+
+    [TestFixture]
+    public class AcquiredImageTest {
+
+        [Test]
+        public void TestAcquiredImage() {
+            AcquiredImage sut = new AcquiredImage();
+            // TODO ...
+        }
+    }
+}
