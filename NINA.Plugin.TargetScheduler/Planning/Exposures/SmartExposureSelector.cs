6adc061eeb56aece3685e1eb587804f6c28350e3
vancurad <152356315+vancurad@users.noreply.github.com>
2025-02-15 23:20:31 -0800

    Prioritize the least captured exposures
    Don't select the first best filter with a high moon score, but look at all of them and check which one of them are noticeably less progressed. Prioritize those first.

--- NINA.Plugin.TargetScheduler/Planning/Exposures/SmartExposureSelector.cs
+++ NINA.Plugin.TargetScheduler/Planning/Exposures/SmartExposureSelector.cs
@@ -2,6 +2,9 @@
 using NINA.Plugin.TargetScheduler.Planning.Interfaces;
 using NINA.Plugin.TargetScheduler.Shared.Utility;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
 
 namespace NINA.Plugin.TargetScheduler.Planning.Exposures {
 
@@ -23,12 +26,41 @@ namespace NINA.Plugin.TargetScheduler.Planning.Exposures {
             IExposure selected = null;
             double highScore = double.MinValue;
 
+            LinkedList<IExposure> qualifyingExposures = new LinkedList<IExposure>();
+            double leastProgressedExposure = Double.MaxValue;
+            int leastProgressedExposureIndex = 0;
+
             foreach (IExposure exposure in target.ExposurePlans) {
                 if (exposure.Rejected) continue;
-                if (exposure.MoonAvoidanceScore > highScore) {
-                    selected = exposure;
+                if (exposure.MoonAvoidanceScore >= highScore) {
+                    double exposureProgress = (double)exposure.Accepted / (double)exposure.Desired;
+                    if (qualifyingExposures.First != null && qualifyingExposures.First.Value.MoonAvoidanceScore == highScore) {
+                        qualifyingExposures.AddLast(exposure);
+                        if (exposureProgress < leastProgressedExposure) {
+                            leastProgressedExposure = exposureProgress;
+                            leastProgressedExposureIndex = qualifyingExposures.Count - 1;
+                        }
+                    } else {
+                        qualifyingExposures = new LinkedList<IExposure>([exposure]);
+                        leastProgressedExposure = exposureProgress;
+                        leastProgressedExposureIndex = 0;
+                    }
                     highScore = exposure.MoonAvoidanceScore;
                 }
+            }
+
+            // If the previous exposure still qualifies and is >= 10% more progressed than the least progressed exposure, take the least progressed
+            // This is to avoid erradic filter switching when all exposures have a mostly balanced progress.
+            // If the previous exposure is no longer qualifying, take the exposure with the least amount of progress and highest moon avoidance score.
+            double previousExposureProgress = previousExposure == null ? Double.MaxValue : (double)previousExposure.Accepted / (double)previousExposure.Desired;
+            if (previousExposure != null && qualifyingExposures.Contains(previousExposure)) {
+                if (previousExposureProgress - leastProgressedExposure > 0.1) {
+                    selected = qualifyingExposures.ElementAt(leastProgressedExposureIndex);
+                } else {
+                    selected = previousExposure;
+                }
+            } else {
+                selected = qualifyingExposures.ElementAt(leastProgressedExposureIndex);
             }
 
             if (selected == null) {
